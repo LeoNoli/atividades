1522e2e94d4aa7e16472932161ceac4e5a3fb2fc
LeoNoli <Nollizleo@gmail.com>
2020-02-19 17:32:29 -0300

    versao 1

---
+++ index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-sacle=1">
+        <title>Inicio</title>
+    </head>
+    <body>
+        <div class="container">
+            <h1>Página com links das minhas atividades</h1>
+        </div>
+    </body>
+</html>
